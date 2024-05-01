29765b3bcf4e980df55797a9218aecc06e636368
S1746234 <S1746234@S0085CG0437YN8.student.HISD.ORG>
2024-05-01 08:34:29 -0500

    background and switchcase

--- src/Game.java
+++ src/Game.java
@@ -7,19 +7,39 @@ public class Game extends JPanel implements Runnable, MouseListener {
     private ImageIcon coinIcon;
     private Sound music;
     private ImageIcon background;
-    private char screen;
-
-
+    private int GS;
+    
     public Game() {
         background = new ImageIcon("Table.jpg");
     	coinCount = 0;
         coinIcon = new ImageIcon("chipp.png"); 
-        screen = 'S';
+        GS=0;
         new Thread(this).start();
         this.setFocusable(true);
         this.addMouseListener(this);
     }
-    
+    // switch case for screens
+    public void Screen (Graphics g2d) {
+    	switch(GS) {
+    	
+    	case 0:
+    		g2d.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this);
+    		 g2d.drawImage(coinIcon.getImage(), getWidth() / 2- 25, getHeight() / 2 - 25, 150, 150, this);
+    	     g2d.setColor(Color.RED);
+    	     g2d.fillRect(20,60,100,50); // button 1
+    	     g2d.setFont(new Font("Arial", Font.BOLD, 20));
+    	     g2d.setColor(Color.WHITE);
+    	     g2d.drawString("gamble", 32, 90);
+    	     g2d.setFont(new Font("Arial", Font.BOLD, 20));
+    	        g2d.drawString("Coins: " + coinCount, 20, 30);
+    		break;
+    	
+    	case 1:
+    		g2d.clearRect(0, 0, getSize().width, getSize().height); 
+    		
+    		break;
+    	}
+    }
     public void run() {
         try {
             while (true) {
@@ -33,25 +53,27 @@ public class Game extends JPanel implements Runnable, MouseListener {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        	g2d.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this);
-        // Draw coin count
-        g2d.setColor(Color.BLACK);
-        g2d.setFont(new Font("Arial", Font.BOLD, 20));
-        g2d.drawString("Coins: " + coinCount, 20, 30);
         // Draw coin icon
-        g2d.drawImage(coinIcon.getImage(), getWidth() / 2- 25, getHeight() / 2 - 25, 150, 150, this);
-    
+        Screen(g2d);
     }
-
     public void mouseClicked(MouseEvent e) {
         // Check if the mouse click is on the coin icon
         int mouseX = e.getX();
         int mouseY = e.getY();
         int coinX = getWidth() / 2 - 25; // X-coordinate of the coin icon
         int coinY = getHeight() / 2 - 25; // Y-coordinate of the coin icon
-
+        
+        int buttonX = 20; // X-coordinate of the button
+        int buttonY = 60; // Y-coordinate of the button
+        int buttonWidth = 100; // Width of the button
+        int buttonHeight = 50; // Height of the button
+   
         if (mouseX >= coinX && mouseX <= coinX + 150 && mouseY >= coinY && mouseY <= coinY + 150) {
             coinCount++;
+        }
+        
+        if (mouseX >= buttonX && mouseX <= buttonX + buttonWidth && mouseY >= buttonY && mouseY <= buttonY + buttonHeight) {
+            GS = 1; // Transition to case 1
         }
     }
 
